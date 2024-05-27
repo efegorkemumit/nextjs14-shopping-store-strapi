f0459b505d0a6487aa9ba8f299e0de37504668aa
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-05-27 23:44:09 +0300

    React Hook Form Login ve Register UI

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,93 @@
+"use client"
 import React from 'react'
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Label } from '@/components/ui/label'
+import Link from 'next/link'
+
+
+const formSchema = z.object({
+  email: z.string().min(2, {
+    message: "Email must be at least 2 characters.",
+  }),
+  password: z.string().min(2, {
+    message: "Password must be at least 2 characters.",
+  }),
+})
 
 const LoginPage = () => {
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password:""
+    },
+  })
+
+  const onSubmit=()=>{
+
+  }
+
   return (
-    <div>asdas</div>
+    <Form {...form}>
+    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-4/5">
+      <FormField
+        control={form.control}
+        name="email"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel className='textone'>Email</FormLabel>
+            <FormControl>
+              <Input placeholder="email[]gmail.com" {...field} />
+            </FormControl>
+            <FormMessage className='validationLogin' />
+          </FormItem>
+        )}
+      />
+
+    <FormField
+        control={form.control}
+        name="password"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel className='textone'>Password</FormLabel>
+            <FormControl>
+              <Input placeholder="Password" type="password" {...field} />
+            </FormControl>
+            <FormMessage className='validationLogin' />
+          </FormItem>
+        )}
+      />
+
+      <Button className='w-full' type="submit">Submit</Button>
+    </form>
+    <div className='mt-8 '>
+      <Label className='flex flex-col items-center'>
+        Dont have an account
+   
+        <Link href="/create-user" 
+        className='text-mycolor3 font-semibold mt-5'>
+        Click here to create a new account
+        </Link>
+
+      </Label>
+
+
+    </div>
+  </Form>
   )
 }
 
