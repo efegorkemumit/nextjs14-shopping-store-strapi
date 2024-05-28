1fd9916f54c350b99ca2fd25b1d4640dbc5d5841
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-05-28 23:50:41 +0300

    Product Collection

--- constans/type.ts
+++ constans/type.ts
@@ -18,3 +18,48 @@ export type Slider={
         };
     }
 }
+
+export type Size={
+    id:number;
+    attributes:{
+        name:string;
+       
+    }
+}
+
+export type Color={
+    id:number;
+    attributes:{
+        name:string;
+       
+    }
+}
+
+export type ProductImage={
+    id:number;
+    data:{
+        attributes:{
+            url:string;
+           
+        }
+    }
+   
+}
+
+export type Product={
+    id:number;
+    attributes:{
+        name:string;
+        description:string;
+        slug:string;
+        mrp:Number;
+        sellingPrice:number;
+        isTop:boolean;
+        recent:boolean;
+        images:ProductImage[];
+        category:Category;
+        colors:Color[];
+        sizes:Size[];
+       
+    }
+}
